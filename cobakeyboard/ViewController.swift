da4b3379162871e18325d23c8e7014385181270b
Shiddiq <syuhadashiddiq97@gmail.com>
2023-12-12 09:26:30 +0700

    Initial Commit

---
+++ cobakeyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cobakeyboard
+//
+//  Created by AIGS on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
